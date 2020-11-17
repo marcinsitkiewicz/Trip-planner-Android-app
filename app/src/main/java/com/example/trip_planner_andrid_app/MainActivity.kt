f22d663b2add5f9952511b4ae1f29f44082941ae
Adrian Budka <226117@edu.p.lodz.pl>
2020-11-17 21:54:03 +0100

    UI version 0.2

--- app/src/main/java/com/example/trip_planner_andrid_app/MainActivity.kt
+++ app/src/main/java/com/example/trip_planner_andrid_app/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.trip_planner_andrid_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("hello git");
-        println("hello! ;C");
+
+        button2.setOnClickListener{
+            startActivity(Intent(this, FlightsListActivity::class.java))
+        }
     }
 }
